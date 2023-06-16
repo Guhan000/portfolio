51a07120b66fbdbe2af0ee2df62a2978f4a47653
Guhan <guhan27062002@gmail.com>
2023-06-16 21:41:00 +0530

    modified

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
+import { styles } from "../styles";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className='flex flex-col flex-wrap justify-center items-center gap-10'>
+      <div variants={textVariant()} className="flex flex-col items-center justify-center">
+        <p className={`${styles.sectionSubText} `}>What I've learned so far</p>
+        <h2 className={`${styles.sectionHeadText}`}>Skills</h2>
+        
+      </div>
+      <div className="flex flex-row flex-wrap justify-center items-center">
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <div >
+            <img src={technology.icon}></img>
+          </div>
         </div>
       ))}
+      </div>
+      
     </div>
   );
 };
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,8 +18,8 @@ const ProjectCard = ({
   live_link
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
+    <div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <div
         options={{
           max: 45,
           scale: 1,
@@ -77,30 +77,17 @@ const ProjectCard = ({
             </button>
           </div>
         </div>
-      </Tilt>
-    </motion.div>
+      </div>
+    </div>
   );
 };
 
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <div variants={textVariant()} className="flex flex-col items-center">
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
-
-      <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          {/* Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively. */}
-        </motion.p>
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
