3534bb8e54c4391806e646455a9393e6f1b2e703
Guhan000 <guhan27062002@gmail.com>
2024-03-01 19:45:41 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
@@ -10,11 +10,9 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        {/* <About /> */}
         <Experience />
         <Tech />
         <Works />
-        {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
